deb391d2a69cab2267c0582f340dd913d46710c6
Don Pavlik <github@donpavlik.net>
2019-12-19 15:21:57 -0500

    Remove of Weather

--- GatesOfJanus/Startup.cs
+++ GatesOfJanus/Startup.cs
@@ -87,7 +87,8 @@ namespace GatesOfJanus
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                c.RoutePrefix = string.Empty;
+                c.InjectStylesheet("/swagger/ui/custom.css");
+                c.RoutePrefix = "docs";
             });
         }
     }
